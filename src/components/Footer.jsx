f7eef504fdebf71d03f5503923c0298cf71120ae
hakeem0114 <hakeem0114@gmail.com>
2023-10-23 03:00:17 -0400

    Finished footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,197 @@
-
+//React Router Imports
+import { Link } from 'react-router-dom';
 
 export default function Footer (){
+
   return (
-    <div>Footer</div>
+    <div className=' 
+    w-full h-44  border-t-[2px] border-b-gray-800 opacity-95 flex flex-col justify-between sticky z-50 top-0 text-gray-700  '>
+    
+      {/* Top Footer */}
+      <div className=' h-40 w-full flex justify-center items-center mx-auto '>
+          <ul className="flex flex-row gap  gap-20">
+              
+              <li className='text-gray-950 font-semibold'>
+                About
+                <ul>
+                    <Link to='#'>
+                        <li className=' mt-5 font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Mission</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >History</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Founder</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Organization</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Key Concerns</li>
+                    </Link>
+
+                </ul>
+              </li>
+
+              <li className='text-gray-950 font-semibold'>
+                Locations
+                <ul>
+                    <Link to='#'>
+                        <li className=' mt-5 font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Mission</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >History</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Founder</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Organization</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Key Concerns</li>
+                    </Link>
+
+                </ul>
+              </li>
+
+              <li className='text-gray-950 font-semibold'>
+                News & Events
+                <ul>
+                    <Link to='#'>
+                        <li className=' mt-5 font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Mission</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >History</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Founder</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Organization</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Key Concerns</li>
+                    </Link>
+
+                </ul>
+
+                
+              </li>
+              <li className='text-gray-950 font-semibold'>
+                Priesthood
+                <ul>
+                    <Link to='#'>
+                        <li className=' mt-5 font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Mission</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >History</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Founder</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Organization</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Key Concerns</li>
+                    </Link>
+
+                </ul>
+
+                
+              </li>
+              <li className='text-gray-950 font-semibold'>
+                Publications
+                <ul>
+                    <Link to='#'>
+                        <li className=' mt-5 font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Mission</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >History</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Founder</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Organization</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Key Concerns</li>
+                    </Link>
+
+                </ul>
+
+                
+              </li>
+
+              <li className='text-gray-950 font-semibold'>
+                Media
+                <ul>
+                    <Link to='#'>
+                        <li className=' mt-5 font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Mission</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >History</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Founder</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Organization</li>
+                    </Link>
+                    <Link to='#'>
+                        <li className='font-normal transform transition duration-300 hover:scale-105 hover:text-gray-950 hover:cursor-pointer'
+                        >Key Concerns</li>
+                    </Link>
+
+                </ul>
+
+                
+              </li>
+          </ul>
+      </div>
+
+
+
+      {/* Bottom Footer */}
+      {/* <div className=' h-20 w-full flex justify-between items-center mx-auto '>
+        <ul>
+          <li>© 2023 Donate: SSPX.ca</li>
+          <li></li>
+        </ul>
+      </div> */}
+
+      
+    </div>
   )
 }
